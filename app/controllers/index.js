1c2ead18b626242cfa691524219c2b9a7cf3e37e
ferni <fertiberti@gmail.com>
2015-04-16 13:07:50 -0300

    Adding some logs.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 var request = require('request');
 
 function handleCoinbase(code) {
+    console.log('Posting to coinbase')
     request.post('https://www.coinbase.com/oauth/token', {
         form: {
             grant_type: 'authorization_code',
@@ -17,6 +18,7 @@ function handleCoinbase(code) {
 module.exports = function(req, res) {
 
     var balance;
+    console.log('req.query.code: ' + req.query.code);
     if (req.query.code) {
         balance = handleCoinbase(req.query.code);
     }
