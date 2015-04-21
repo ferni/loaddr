2c120365dc888bd6752a4f79bad89eac52798cb9
ferni <fertiberti@gmail.com>
2015-04-21 17:06:49 -0300

    Replaced url with mongolab uri.

--- config/database.js
+++ config/database.js
@@ -5,6 +5,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://ferni:asdf@proximus.modulusmongo.net:27017/Xym9osuq' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : process.env.MONGOLAB_URI // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
