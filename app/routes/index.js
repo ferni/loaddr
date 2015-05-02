6a8e9f250a66c1aa6dbdc865b00dd1924c8dff5c
ferni <fertiberti@gmail.com>
2015-05-02 19:34:15 -0300

    Redirect to home on sign up.

--- app/routes/index.js
+++ app/routes/index.js
@@ -19,7 +19,7 @@ module.exports = function(app, passport) {
     });
 
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile',
+        successRedirect : '/',
         failureRedirect : '/signup',
         failureFlash : true
     }));
