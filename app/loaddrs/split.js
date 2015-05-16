338bbac9fd25b2c9690d980f6876e5ee217c2228
ferni <fertiberti@gmail.com>
2015-05-15 23:31:00 -0300

    Split loader sliders.

---
+++ app/loaddrs/split.js
@@ -0,0 +1,38 @@
+var wallet = require('../wallet');
+var Promise = require('bluebird');
+var bitcore = require('bitcore');
+
+module.exports = {
+    onIncoming: function (amount, loaddr) {
+        return wallet.send({
+            loaddr: loaddr,
+            outputs: [{
+                address: loaddr.settings.destinationAddress,
+                amount: amount - wallet.fee
+            }]
+        }).then(function () {
+            loaddr.log('Sent funds to ' + loaddr.settings.destinationAddress);
+        });
+    },
+    validateSettings: function (settings) {
+        return new Promise(function(resolve, reject){
+            if(settings.destinationAddress) {
+                try {
+                    bitcore.Address.fromString(settings.destinationAddress);
+                    resolve({});
+                } catch(e) {
+                    resolve({errors: ['Invalid address format.']});
+                }
+            } else {
+                resolve({errors: ['Please enter a bitcoin address to redirect the funds to.']});
+            }
+        });
+    },
+    createForm: function () {
+        return '{external}';
+    },
+    settingsForm: function(settings) {
+        //TODO: Make the settings editable (ajax)
+        return 'Destination: <strong>' + settings.destinationAddress+ '</strong>';
+    }
+};
